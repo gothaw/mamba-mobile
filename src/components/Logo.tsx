1992c0da6f9d510d75b7523d0749fa02afd75560
gothaw <rad.soltan@gmail.com>
2023-01-05 23:29:17 +0000

    Added styling for the menu button

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react";
-import { Dimensions, Image, View } from "react-native";
+import { Image, View } from "react-native";
 
 interface Props {
   style: object
@@ -8,20 +8,10 @@ interface Props {
 const Logo: FunctionComponent<Props> = (props) => {
   const {style} = props;
 
-  // todo https://www.npmjs.com/package/react-native-responsive-screen
-  const width = Dimensions.get("window").width * 0.5;
-  const height = Dimensions.get("window").height * 0.5;
-
   return (
     <View>
       <Image
-        style={
-          {
-            ...style,
-            maxWidth: width,
-            maxHeight: height
-          }
-        }
+        style={{...style}}
         source={require("../assets/img/mamba_logo_color.png")}
       />
     </View>
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,13 +1,17 @@
+import { Dimensions } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import { ContextInterface } from "../types";
 
 import { Font, FontSize, Palette } from "./default";
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
 export const theme : ContextInterface = {
   container: {
     flex: 1,
-    position: "relative"
+    position: "relative",
   },
   viewWrapper: {
     flex: 1,
@@ -16,18 +20,44 @@ export const theme : ContextInterface = {
     position: "relative"
   },
   mainMenuLogo: {
+    width: windowWidth * 0.5,
+    height: windowHeight * 0.5,
     resizeMode: "contain"
   },
   mainMenuButton: {
-    width: "50%",
-    alignItems: "center"
+    marginTop: 15,
+    borderWidth: 4,
+    borderColor: Palette.Yellow,
+    backgroundColor: Palette.DarkGreen,
+    borderRadius: 15,
+    height: "15%",
+    width: "25%",
+    // Shadow
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
   },
   mainMenuButtonText: {
+    height: RFValue(FontSize.L),
+    boxSizing: "border-box",
+    textAlign: "center",
+    padding: 0,
+    margin: 0,
     color: Palette.Yellow,
     fontFamily: Font.LuckiestGuy,
     fontSize: RFValue(FontSize.L),
+    letterSpacing: 1.5,
     textShadowColor: "rgba(0, 0, 0, 1)",
-    textShadowOffset: {width: -2, height: 2},
+    textShadowOffset: {width: -1.5, height: 1.5},
     textShadowRadius: 1,
+    lineHeight: RFValue(FontSize.L),
+    transform: [
+      { translateY: windowHeight * 0.036 }
+    ]
   }
 };
