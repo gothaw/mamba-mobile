c7a80f7ffcd7b6774039082be292b6b361c38840
gothaw <rad.soltan@gmail.com>
2023-02-04 22:37:33 +0000

    Fixing tests

--- src/client/__tests__/App.test.tsx
+++ src/client/__tests__/App.test.tsx
@@ -4,8 +4,8 @@ import App from "../App";
 
 describe("App", () => {
 
-  it("should render a main controller without crashing", () => {
-    const shallow = renderer.create(<App/>).toJSON();
+  it("should render a main controller without crashing", async () => {
+    renderer.create(<App/>).toJSON();
 
     expect(0).toBe(0);
   });
