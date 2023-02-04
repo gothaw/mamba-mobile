d839f7ad6480171c38d5fcffe81787335b139a48
gothaw <rad.soltan@gmail.com>
2023-02-04 23:07:15 +0000

    Fixing tests using render

--- src/client/__tests__/App.test.tsx
+++ src/client/__tests__/App.test.tsx
@@ -1,11 +1,12 @@
-import renderer from "react-test-renderer";
+import { act, render } from "@testing-library/react-native";
 
+import MainMenu from "../../views/MainMenu";
 import App from "../App";
 
 describe("App", () => {
-
+  
   it("should render a main controller without crashing", async () => {
-    renderer.create(<App/>).toJSON();
+    render(<App/>);
 
     expect(0).toBe(0);
   });
