eecb1a671014a7f1077a62fc6b014eb7b1fba90f
gothaw <rad.soltan@gmail.com>
2023-02-05 15:52:04 +0000

    Added MainController tests.

---
+++ src/client/__tests__/MainController.test.tsx
@@ -0,0 +1,21 @@
+import { render, waitFor } from "@testing-library/react-native";
+
+import MainController from "../MainController";
+
+describe("MainController", () => {
+  let component;
+
+  beforeEach(() => {
+    component = render(<MainController/>);
+  });
+
+  it("should render without crashing", async () => {
+    await waitFor(() => {
+      expect(component.toJSON()).toBeTruthy();
+    });
+  });
+
+  it("should not render if not ready", () => {
+    expect(component.toJSON()).toBe(null);
+  });
+});
