e200ae88fe45d9a5d3917e2ec1cf38b0a46fdf80
gothaw <rad.soltan@gmail.com>
2023-02-11 16:27:48 +0000

    Added test for Game.

---
+++ src/views/__tests__/Game.test.tsx
@@ -0,0 +1,27 @@
+import { NativeRouter, Route, Routes } from "react-router-native";
+import { act, render, waitFor } from "@testing-library/react-native";
+
+import { ContextProvider } from "../../client/ContextProvider";
+import Game from "../Game";
+
+describe("Game", () => {
+  it("should render without crashing", async () => {
+    const menu = render(
+      <ContextProvider>
+        <NativeRouter>
+          <Routes>
+            <Route path={"/"} element={<Game/>} />
+          </Routes>
+        </NativeRouter>
+      </ContextProvider>
+    );
+
+    await act(async () => {
+      await new Promise(resolve => setImmediate(resolve));
+    });
+
+    await waitFor(() => {
+      expect(menu.toJSON()).toMatchSnapshot();
+    });
+  });
+});
