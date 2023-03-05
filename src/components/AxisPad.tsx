ed76253d54200e4f0e87d317f5fbdd9dcd3506e0
gothaw <rad.soltan@gmail.com>
2023-03-05 23:11:30 +0000

    Adding some basic documentation and renaming variables to be more semantic.

--- src/components/AxisPad.tsx
+++ src/components/AxisPad.tsx
@@ -2,28 +2,58 @@ import React, { ReactNode } from "react";
 import { Animated, Easing, View } from "react-native";
 import Value = Animated.Value;
 
+/**
+ * Axis pad props
+ *
+ * autoCenterPad    auto centres pad on touch event start
+ * size             pad size
+ * handlerSize      size of the handler
+ * step             defines a step in which handler can change its position on the pad
+ * onValue          a callback from props that is fired on touch start, end and move as a parameters it takes an object with x and y coordinates of the handler
+ * freezeOnRelease  defines if the handler should be frozen in place after releasing and not reset to initial position
+ * wrapperStyle     style of the pad
+ * handlerStyle     style of the handler
+ * children         components that can be passed in props
+ */
 interface Props {
-  autoCenter?: boolean,
+  autoCenterPad?: boolean,
   size?: number,
   handlerSize?: number,
   step?: number,
-  onValue: (object) => void,
-  resetOnRelease?: boolean,
+  onValue: (object: {x: number, y: number}) => void,
+  freezeOnRelease?: boolean,
   wrapperStyle?: object,
   handlerStyle?: object,
   children?: ReactNode
 }
 
+/**
+ * Axis pad state interface
+ *
+ * identifier         event identifier
+ * padX               x coordinate of the centre of the pad. If 0 then pad is in initial position on x-axis. This is used when autoCenterPad is true.
+ * padY               y coordinate of the centre of the pad. If 0 then pad is in initial position on y-axis. This is used when autoCenterPad is true.
+ * touchX             x coordinate of the touch event. This is relative to the screen.
+ * touchY             y coordinate of the touch event. This is relative to the screen.
+ * x                  coordinate (x) of the centre of the handler. If 0 then pad is in the centre of the pad on x-axis.
+ * y                  coordinate (y) of the centre of the handler. If 0 then pad is in the centre of the pad on y-axis.
+ * onReleaseX         x coordinate of the centre of the handler when touch event finished. If 0, then handler returns to initial position on x-axis.
+ * onReleaseY         y coordinate of the centre of the handler when touch event finished. If 0, then handler returns to initial position on y-axis.
+ * width              pad width
+ * height             pad height
+ * handler            diameter of the handler
+ * step               defines a step in which handler can change its position on the pad
+ */
 interface State {
   identifier: number,
-  cx: number,
-  cy: number,
-  sx: number,
-  sy: number,
-  px: number,
-  py: number,
-  dx: number,
-  dy: number,
+  padX: number,
+  padY: number,
+  touchX: number,
+  touchY: number,
+  x: number,
+  y: number,
+  onReleaseX: number,
+  onReleaseY: number,
   width: number,
   height: number,
   handler: number,
@@ -31,34 +61,34 @@ interface State {
 }
 
 class AxisPad extends React.Component<Props, State> {
-  anim_cx: Value;
-  anim_cy: Value;
-  anim_px: Value;
-  anim_py: Value;
+  animatedPadX: Value;
+  animatedPadY: Value;
+  animatedX: Value;
+  animatedY: Value;
   wrapperElement: Animated.LegacyRef<View> | View;
   private handlerElement: Animated.LegacyRef<View> | View;
 
   constructor(props: Props) {
     super(props);
-    this.anim_cx = new Animated.Value(0);
-    this.anim_cy = new Animated.Value(0);
-    this.anim_px = new Animated.Value(0);
-    this.anim_py = new Animated.Value(0);
+    this.animatedPadX = new Animated.Value(0);
+    this.animatedPadY = new Animated.Value(0);
+    this.animatedX = new Animated.Value(0);
+    this.animatedY = new Animated.Value(0);
 
     this.state = {
-      cx: 0,
-      cy: 0,
-      dx: 0,
-      dy: 0,
       handler: props.handlerSize ? props.handlerSize : 100,
       height: props.size ? props.size : 300,
       identifier: 0,
-      px: 0,
-      py: 0,
+      onReleaseX: 0,
+      onReleaseY: 0,
+      padX: 0,
+      padY: 0,
       step: props.step ? props.step : 0,
-      sx: 0,
-      sy: 0,
-      width: props.size ? props.size : 300
+      touchX: 0,
+      touchY: 0,
+      width: props.size ? props.size : 300,
+      x: 0,
+      y: 0
     };
 
     this.onTouchStart = this.onTouchStart.bind(this);
@@ -74,20 +104,20 @@ class AxisPad extends React.Component<Props, State> {
 
   centerAnimate() {
     Animated.timing(
-      this.anim_cx,
+      this.animatedPadX,
       {
         duration: 300,
         easing: Easing.elastic(1),
-        toValue: this.state.cx,
+        toValue: this.state.padX,
         useNativeDriver: true
       }
     ).start();
     Animated.timing(
-      this.anim_cy,
+      this.animatedPadY,
       {
         duration: 300,
         easing: Easing.elastic(1),
-        toValue: this.state.cy,
+        toValue: this.state.padY,
         useNativeDriver: true
       }
     ).start();
@@ -95,20 +125,20 @@ class AxisPad extends React.Component<Props, State> {
 
   animate() {
     Animated.timing(
-      this.anim_px,
+      this.animatedX,
       {
         duration: 50,
         easing: Easing.out(Easing.exp),
-        toValue: this.state.px,
+        toValue: this.state.x,
         useNativeDriver: true
       }
     ).start();
     Animated.timing(
-      this.anim_py,
+      this.animatedY,
       {
         duration: 50,
         easing: Easing.out(Easing.exp),
-        toValue: this.state.py,
+        toValue: this.state.y,
         useNativeDriver: true
       }
     ).start();
@@ -136,12 +166,12 @@ class AxisPad extends React.Component<Props, State> {
   }
 
   centerPosition(pageX, pageY) {
-    (this.handlerElement as View).measure((fx, fy, width, height, px, py) => {
-      const cx = pageX - px - width / 2;
-      const cy = pageY - py - height / 2;
+    (this.handlerElement as View).measure((fx, fy, width, height, x, y) => {
+      const padX = pageX - x - width / 2;
+      const padY = pageY - y - height / 2;
       this.setState({
-        cx,
-        cy
+        padX,
+        padY
       }, this.centerAnimate);
     });
   }
@@ -165,15 +195,15 @@ class AxisPad extends React.Component<Props, State> {
     if (typeof identifier === "number" && touchItem) {
       const { pageX, pageY } = touchItem;
 
-      if (this.props.autoCenter) {
+      if (this.props.autoCenterPad) {
         this.centerPosition(pageX, pageY);
       }
 
-      this.sendValue(this.state.px, this.state.py);
+      this.sendValue(this.state.x, this.state.y);
       this.setState({
         identifier,
-        sx: pageX,
-        sy: pageY
+        touchX: pageX,
+        touchY: pageY
       });
     }
   }
@@ -183,36 +213,36 @@ class AxisPad extends React.Component<Props, State> {
     if (touchItem) {
       const { pageX, pageY } = touchItem;
 
-      const pxStatic = pageX - this.state.sx + this.state.dx;
-      const pyStatic = pageY - this.state.sy + this.state.dy;
+      const handlerXStatic = pageX - this.state.touchX + this.state.onReleaseX;
+      const handlerYStatic = pageY - this.state.touchY + this.state.onReleaseY;
 
-      const px = this.limiter(pxStatic, pyStatic);
-      const py = this.limiter(pyStatic, pxStatic);
-      if (px && py) {
-        this.sendValue(px, py);
-        this.setState({ px, py }, this.animate);
+      const x = this.limiter(handlerXStatic, handlerYStatic);
+      const y = this.limiter(handlerYStatic, handlerXStatic);
+      if (x && y) {
+        this.sendValue(x, y);
+        this.setState({ x, y }, this.animate);
       }
     }
   }
 
   onTouchEnd() {
-    let { px, py } = this.state;
-    if (this.props.resetOnRelease) {
-      px = 0;
-      py = 0;
+    let { x, y } = this.state;
+    if (!this.props.freezeOnRelease) {
+      x = 0;
+      y = 0;
     }
-    const dx = px;
-    const dy = py;
-    this.sendValue(px, py);
+    const onReleaseX = x;
+    const onReleaseY = y;
+    this.sendValue(x, y);
     this.setState({
-      cx: 0,
-      cy: 0,
-      dx,
-      dy,
-      px,
-      py,
-      sx: 0,
-      sy: 0
+      onReleaseX,
+      onReleaseY,
+      padX: 0,
+      padY: 0,
+      touchX: 0,
+      touchY: 0,
+      x,
+      y
     }, () => {
       this.centerAnimate();
       this.animate();
@@ -221,8 +251,8 @@ class AxisPad extends React.Component<Props, State> {
 
   onTouchCancel() {
     this.setState({
-      cx: 0,
-      cy: 0
+      padX: 0,
+      padY: 0
     }, this.centerAnimate);
   }
 
@@ -235,9 +265,9 @@ class AxisPad extends React.Component<Props, State> {
                      style={[AxisPadStyle.wrapper, this.props.wrapperStyle ? this.props.wrapperStyle : {}, {
                        height: this.state.height,
                        transform: [{
-                         translateX: this.anim_cx
+                         translateX: this.animatedPadX
                        }, {
-                         translateY: this.anim_cy
+                         translateY: this.animatedPadY
                        }],
                        width: this.state.width
                      }]}
@@ -247,9 +277,9 @@ class AxisPad extends React.Component<Props, State> {
           style={[AxisPadStyle.handler, this.props.handlerStyle ? this.props.handlerStyle : {}, {
             height: this.state.handler,
             transform: [{
-              translateX: this.state.px
+              translateX: this.state.x
             }, {
-              translateY: this.state.py
+              translateY: this.state.y
             }],
             width: this.state.handler
           }]}>
